8a31a279c5b24bd8fd72f52ec19d6731dbe79398
Denis Lieskov <62790618+benjaminfein@users.noreply.github.com>
2022-02-19 13:55:00 +0200

    Added new class Scan, which can show how to scan enter from console

---
+++ src/learning/Scan.java
@@ -0,0 +1,29 @@
+package learning;
+import java.util.Scanner;
+
+public class Scan {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter your name : ");
+
+        String name = console.nextLine();
+
+        System.out.println(name + ", enter your real balance : ");
+
+        int balance = console.nextInt();
+
+        int outBalance = balance - 100;
+        System.out.println(name + ", your balance after purchase will be : " + outBalance);
+
+        System.out.println(name + ", enter Pi number : ");
+
+        double pi = console.nextDouble();
+
+        if(pi == 3.1415926535) {
+            System.out.println("You entered the correct number");
+            }else{
+            System.out.println("You entered incorrect number, change it");
+            }
+        }
+    }
+
