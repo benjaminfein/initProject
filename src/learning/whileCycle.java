a8bc712cd1b1aa9f65c4a8e2df625c72f08c0c57
Denis Lieskov <62790618+benjaminfein@users.noreply.github.com>
2022-02-21 12:38:17 +0200

    class whileCycle was edited

--- src/learning/whileCycle.java
+++ src/learning/whileCycle.java
@@ -1,9 +1,34 @@
 package learning;
 
+import java.util.Scanner;
+
 public class whileCycle {
     public static void main(String[] args) {
-        int k = 1;
-        while (k <= 50)
-            System.out.println(k++);
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please, enter your name : ");
+        String name = console.nextLine();
+        int agreement = 0;
+        double price = 15;
+        while(agreement == 0 || agreement == 2) {
+            System.out.println("Ok, now we can suggest our special sale for client like you! It's a Ford Mustang just about " + price + "k$! Just write '1' if you agree, " +
+                    "'2' if you want to think or '0' if you don't agree : ");
+            agreement = console.nextInt();
+            if (agreement == 1) {
+                System.out.println("Ok, " + name + ", this Mustang is yours! You buy it for " + price + "k$");
+                break;
+            }
+            if (agreement == 0 || price <= 7.5){
+                System.out.println("Deal failed. Have a good day, " + name + "!");
+                break;
+            }
+            if (agreement == 2) {
+                price -= 1.5;
+                System.out.println("Ok, " + name + ", now we can suggest same Mustang for only " + price + "k$. What do you think about that? Just write '1' if you agree, " +
+                        "'2' if you want to think or '0' if you don't agree : ");
+                agreement = console.nextInt();
+                continue;
+            }
+        }
+
     }
 }
