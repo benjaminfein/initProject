598b5a7742a1773982e92e78112ca8790c996403
Denis Lieskov <62790618+benjaminfein@users.noreply.github.com>
2022-02-21 13:02:07 +0200

    added new class forCycle

---
+++ src/learning/forCycle.java
@@ -0,0 +1,16 @@
+package learning;
+
+import java.util.Scanner;
+
+public class forCycle {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Enter the variable, from which you want to calculate the factorial : ");
+        int f = console.nextInt();
+        System.out.println("You entered " + f);
+        int k = 1;
+        for (int i = 1; i <= f; i++)
+            k = k * i;
+        System.out.println("Result : " + f + "! = " + k);
+    }
+}
